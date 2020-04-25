bdad36e1e20cc358d0e55041336b4d74f22097fb
Gautham Anantharaj <gauthama1996@gmail.com>
2020-04-26 00:05:48 +0530

    Landing Page- Post route to submit

--- controllers/landing.js
+++ controllers/landing.js
@@ -2,3 +2,9 @@ exports.getLanding = function (req, res, next) {
     //res.render call the index file in views
     res.render('landing', { title: 'Express' });
 }
+
+exports.submitLead = function (req, res, next) {
+    //in app.js urlencode is used to retrieve the body inputs
+   console.log("Lead Email : ", req.body.leadEmail);
+   res.redirect("/");//redirect back to the landing page once submitted
+}
--- routes/index.js
+++ routes/index.js
@@ -4,5 +4,6 @@ var router = express.Router();
 let index = require("../controllers/landing");
 /* GET home page. */
 router.get('/', index.getLanding);
+router.post('/', index.submitLead)
 
 module.exports = router;
