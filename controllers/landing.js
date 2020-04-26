6730088040318366d3cf1ae628cf007b6f1f0d95
Gautham Anantharaj <gauthama1996@gmail.com>
2020-04-26 19:55:58 +0530

    added sequelize and pg packages

--- controllers/landing.js
+++ controllers/landing.js
@@ -1,5 +1,5 @@
 exports.getLanding = function (req, res, next) {
-    //res.render call the index file in views
+    //res.render call the landing file in views
     res.render('landing', { title: 'Express' });
 }
 
