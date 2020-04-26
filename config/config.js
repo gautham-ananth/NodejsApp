44732ae34d112e24d6fd82d60b5c037c316ffd67
Gautham Anantharaj <gauthama1996@gmail.com>
2020-04-26 20:53:25 +0530

    Initialise sequelize in bin/www

--- config/config.js
+++ config/config.js
@@ -4,8 +4,7 @@ module.exports = {
     "password": '123.456',
     "database": "express-mvp-db",
     "host": "127.0.0.1",
-    "dialect": "postgress",
-    "operatorsAliases": false,
+    "dialect": "postgres",
     "port": 5432
   },
   "test": {
