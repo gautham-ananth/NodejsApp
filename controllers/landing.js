53a927f7e785a93803668548f9bb6aba098479b4
Gautham Anantharaj <gauthama1996@gmail.com>
2020-04-26 20:59:37 +0530

    Save leads collected on Landing Page

--- controllers/landing.js
+++ controllers/landing.js
@@ -1,10 +1,16 @@
+//models give access to sequelize methods
+const models = require('../models');
+
 exports.getLanding = function (req, res, next) {
     //res.render call the landing file in views
     res.render('landing', { title: 'Express' });
 }
 
 exports.submitLead = function (req, res, next) {
-    //in app.js urlencode is used to retrieve the body inputs
-   console.log("Lead Email : ", req.body.leadEmail);
-   res.redirect("/");//redirect back to the landing page once submitted
+    //in app.js urlencode is used to retrieve the body inputs   console.log("Lead Email : ", req.body.leadEmail);
+    return models.Lead.create({
+        email: req.body.leadEmail,
+    }).then(lead => {
+        res.redirect("/")//redirect back to the landing page once submitted
+    })
 }
